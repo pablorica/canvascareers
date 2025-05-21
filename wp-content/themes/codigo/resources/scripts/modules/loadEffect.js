75f2edf34ac3c078aceccf1583bc3781dec248b8
Pablo Rica <pabloricaaldeguer@gmail.com>
2025-05-21 20:42:10 +0200

    Remove debug code

--- wp-content/themes/codigo/resources/scripts/modules/loadEffect.js
+++ wp-content/themes/codigo/resources/scripts/modules/loadEffect.js
@@ -262,8 +262,8 @@ const loadEffect = () => {
   }
 
   //Debug
-  body.classList.add('load-effect');
-  launchPreloader();
+  //body.classList.add('load-effect');
+  //launchPreloader();
 };
 
 export default loadEffect;
