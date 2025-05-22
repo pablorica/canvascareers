ef91d12d3db83758086fc91cff74cdb1f2d209ec
Pablo Rica <pabloricaaldeguer@gmail.com>
2025-05-22 17:53:29 +0200

    Jobs Feedback

--- wp-content/themes/codigo/resources/views/template-jobs.blade.php
+++ wp-content/themes/codigo/resources/views/template-jobs.blade.php
@@ -136,7 +136,7 @@ if ($category) {
                   </div>
                 </div>
 
-                <div class="grid grid-cols-12">
+                <div class="w-full grid grid-cols-12">
                   @if(get_field('start_column', $job))
                     <div class="
                       col-span-12 md:col-span-7 lg:col-span-5 xl:col-span-3 2xl:col-span-4
